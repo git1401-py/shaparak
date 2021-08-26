642f4ed0f3a2f5a107ad10602bc05fe8172e5c17
kazhal1400 <quick.k1399@gmail.com>
2021-08-26 17:24:16 +0430

    edit about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,67 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container-fluid text-end" style="background: lightgray">
+    <div class="row">
+      <div class="col-sm-1 col-md-2 col-lg-3"></div>
+      <div class="col-sm-11 col-md-10 col-lg-9" style="font-weight:400">
+        <div class="pt-5 pb-2 fs-4" style="font-weight:800">درباره ما</div>
+        <hr />
+        <img
+          src="../../public/img/aboutus.png"
+          style=" height:200px"
+          class="my-4 w-100 border border-2"
+        />
+        <p class="pb-4"
+          >شرکت شاپرک با توجه به دلایل متعددی که مهمترین آنها عبارت بودند از
+          دشوار شدن کنترل و نظارت، ناکافی بودن کنترل‌ها و تخصیص غیر بهینه منابع
+          در صنعت پرداخت کشور و بر اساس چارچوب مصوبه مورخ 1389/12/25 شورای پول و
+          اعتبار ایجاد گردید.</p
+        >
+        <div class="fw-bold">ماموریت </div>
+        <p class="mb-4"
+          >ماموریت ما تنظیم مقررات و توسعه راهبردی زیر ساخت های فناورانه و امن
+          در زیست بوم های مبتنی بر پرداخت و خدمات مالی است. قابلیت های منحصر به
+          فرد ما، زمینه ی تنظیم گری موثر ، تطبیق بازیگران و تجربه مطلوب کاربران
+          را فراهم نموده و فعالیت فناوران مالی را تسهیل می کند.</p
+        >
+        <div class="fw-bold">چشم انداز </div>
+        <p class="mb-4"
+          >در جایگاه پیش ران رشد و ارتقاء زیست بوم های مبتنی بر پرداخت و خدمات
+          مالی ، هم ردیف با الگوهای موفق جهانی ، کانون اعتماد بازیگران و کاربران
+          و نقطه اتکاء فناوران مالی هستیم.
+        </p>
+        <div class="fw-bold">اهداف راهبردی شاپرک </div>
+        <ul>
+          <li>
+            <span class="fw-bold">هدف 1 :</span
+            ><span>ارتقاء کیفیت تنظیم گری، امنیت و تطبیق</span>
+          </li>
+          <li>
+            <span class="fw-bold">هدف 2 :</span
+            ><span>توسعه ی نظارت هوشمند </span>
+          </li>
+          <li>
+            <span class="fw-bold">هدف 3 :</span
+            ><span> تقویت بستر های توسعه و نوآوری</span>
+          </li>
+          <li>
+            <span class="fw-bold">هدف 4 :</span
+            ><span
+              >توسعه ی مشارکت ها و فرصت های خلق ارزش در زیست بوم های جدید
+            </span>
+          </li>
+          <li>
+            <span class="fw-bold">هدف 5 :</span
+            ><span>توسعه ی سرمایه انسانی و قابلیت های راهبردی شرکت</span>
+          </li>
+        </ul>
+      </div>
+      <div class="col-sm-1 col-md-2 col-lg-3"></div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
